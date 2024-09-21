6e54843c91bc6f6c996b9bd8c283b480131a67e3
Bartlomiej Mika <bart@mikasoftware.com>
2024-09-21 00:02:52 -0400

    Added more security features

--- native/desktop/comiccoin/internal/inputport/p2p/host.go
+++ native/desktop/comiccoin/internal/inputport/p2p/host.go
@@ -4,9 +4,13 @@ import (
 	"context"
 	"fmt"
 	"log/slog"
+	"time"
 
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p/core/host"
+	"github.com/libp2p/go-libp2p/p2p/net/connmgr"
+	"github.com/libp2p/go-libp2p/p2p/security/noise"
+	libp2ptls "github.com/libp2p/go-libp2p/p2p/security/tls"
 	ma "github.com/multiformats/go-multiaddr"
 )
 
@@ -34,9 +38,46 @@ func (node *nodeInputPort) newHostWithPredictableIdentifier() (host.Host, error)
 	// Loadup our p2p host server.
 	//
 
+	connmgr, err := connmgr.NewConnManager(
+		100, // Lowwater
+		400, // HighWater,
+		connmgr.WithGracePeriod(time.Minute),
+	)
+	if err != nil {
+		panic(err)
+	}
+
 	basicHost, err := libp2p.New(
-		libp2p.ListenAddrStrings(fmt.Sprintf("/ip4/127.0.0.1/tcp/%d", node.cfg.Peer.ListenPort)),
+		libp2p.ListenAddrStrings(
+			fmt.Sprintf("/ip4/127.0.0.1/tcp/%d", node.cfg.Peer.ListenPort), // regular tcp connections
+			fmt.Sprintf("/ip4/127.0.0.1/udp/%d/quic-v1", node.cfg.Peer.ListenPort),
+		),
+		// Use the keypair we generated
 		libp2p.Identity(priv),
+		// support TLS connections
+		libp2p.Security(libp2ptls.ID, libp2ptls.New),
+		// support noise connections
+		libp2p.Security(noise.ID, noise.New),
+		// support any other default transports (TCP)
+		libp2p.DefaultTransports,
+		// Let's prevent our peer from having too many
+		// connections by attaching a connection manager.
+		libp2p.ConnectionManager(connmgr),
+
+		// Attempt to open ports using uPNP for NATed hosts.
+		// libp2p.NATPortMap(),// TODO: UNCOMMENT WHEN WE ARE READY TO LAUNCH THIS APP INTO PRODUCTION
+
+		// If you want to help other peers to figure out if they are behind
+		// NATs, you can launch the server-side of AutoNAT too (AutoRelay
+		// already runs the client)
+		//
+		// This service is highly rate-limited and should not cause any
+		// performance issues.
+		// libp2p.EnableNATService(), // TODO: UNCOMMENT WHEN WE ARE READY TO LAUNCH THIS APP INTO PRODUCTION
+
+		// DEVELOPERS NOTE:
+		// See more options via this link:
+		// https://github.com/libp2p/go-libp2p/blob/master/examples/libp2p-host/host.go#L56
 	)
 	if err != nil {
 		node.logger.Error("failed starting basic host",
