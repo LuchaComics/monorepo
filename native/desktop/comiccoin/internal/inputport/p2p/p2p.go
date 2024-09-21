19aa5a6a348db7f442d4706386cdf8f4c82c2697
Bartlomiej Mika <bart@mikasoftware.com>
2024-09-20 23:29:01 -0400

    Working prototype of dht based p2p service

--- native/desktop/comiccoin/internal/inputport/p2p/p2p.go
+++ native/desktop/comiccoin/internal/inputport/p2p/p2p.go
@@ -7,7 +7,6 @@ import (
 	"log/slog"
 	"time"
 
-	dht "github.com/libp2p/go-libp2p-kad-dht"
 	"github.com/libp2p/go-libp2p/core/host"
 	"github.com/libp2p/go-libp2p/core/peer"
 	drouting "github.com/libp2p/go-libp2p/p2p/discovery/routing"
@@ -34,17 +33,11 @@ func NewInputPort(
 	kp keypair_ds.KeypairStorer,
 	bc blockchain_c.BlockchainController,
 ) inputport.InputPortServer {
+	ctx := context.Background()
 
-	// DEVELOPERS NOTE:
-	// A Host contains all the core functionalities you require connecting
-	// one peer to another. A Host contains an ID which is the identity of
-	// a peer. The Host also contains a Peerstore which is like an address
-	// book. With a Host you can Connect to other peers and create Streams
-	// between them. A Stream represents a communication channel between
-	// two peers in a libp2p network.
-	//
-	// Link :https://ldej.nl/post/building-an-echo-application-with-libp2p/
-
+	// Begin our function by initializing the defaults for our peer-to-peer (p2p)
+	// node and then the rest of this function pertains to setting up a p2p
+	// network to utilize in our app.
 	node := &nodeInputPort{
 		cfg:                  cfg,
 		logger:               logger,
@@ -67,30 +60,13 @@ func NewInputPort(
 	// client because we want each peer to maintain its own local copy of the
 	// DHT, so that the bootstrapping node of the DHT can go down without
 	// inhibiting future peer discovery.
-	ctx := context.Background()
-	var options []dht.Option
-	bootstrapPeers := make([]peer.AddrInfo, len(node.cfg.Peer.BootstrapPeers))
-	for i, addr := range node.cfg.Peer.BootstrapPeers {
-		peerinfo, _ := peer.AddrInfoFromP2pAddr(addr)
-		bootstrapPeers[i] = *peerinfo
-	}
-	if len(bootstrapPeers) == 0 {
-		options = append(options, dht.Mode(dht.ModeServer))
-		node.logger.Info("Running p2p node as dht server")
-	}
-	options = append(options, dht.BootstrapPeers(bootstrapPeers...))
-
-	kademliaDHT, err := dht.New(ctx, host, options...)
+	//
+	// Source: https://github.com/libp2p/go-libp2p/blob/master/examples/chat-with-rendezvous/chat.go#L112
+	kademliaDHT, err := node.newKademliaDHT(ctx)
 	if err != nil {
-		panic(err)
-	}
-
-	// Bootstrap the DHT. In the default configuration, this spawns a Background
-	// thread that will refresh the peer table every five minutes.
-	logger.Debug("Bootstrapping the DHT",
-		slog.String("RendezvousString", node.cfg.Peer.RendezvousString))
-	if err = kademliaDHT.Bootstrap(ctx); err != nil {
-		panic(err)
+		logger.Debug("Failed creating new kademlia dht",
+			slog.Any("error", err))
+		log.Fatal(err)
 	}
 
 	// Wait a bit to let bootstrapping finish (really bootstrap should block until it's ready, but that isn't the case yet.)
