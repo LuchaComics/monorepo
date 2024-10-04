adedfe8ac14ebef27d9a0b76af36453760f72c3f
Bartlomiej Mika <bart@mikasoftware.com>
2024-10-04 01:25:40 -0400

    Added more useful comments.

--- native/desktop/comiccoin/pkg/db/leveldb/leveldb.go
+++ native/desktop/comiccoin/pkg/db/leveldb/leveldb.go
@@ -13,16 +13,23 @@ import (
 	"github.com/LuchaComics/monorepo/native/desktop/comiccoin/pkg/db"
 )
 
+// keyValueStorerImpl implements the db.Database interface.
+// It uses a LevelDB database to store key-value pairs.
 type keyValueStorerImpl struct {
+	// The LevelDB database instance.
 	db *leveldb.DB
 }
 
+// dbDirName is the name of the directory where the database is stored.
 const dbDirName = "db"
 
+// GetDBDirPath returns the path to the database directory.
 func GetDBDirPath(dataDir string) string {
 	return filepath.Join(dataDir, dbDirName)
 }
 
+// NewDatabase creates a new instance of the keyValueStorerImpl.
+// It opens the database file at the specified path and returns an error if it fails.
 func NewDatabase(dataDir string, logger *slog.Logger) db.Database {
 	if dataDir == "" {
 		log.Fatal("cannot have empty dir")
@@ -36,17 +43,26 @@ func NewDatabase(dataDir string, logger *slog.Logger) db.Database {
 	}
 }
 
+// Get retrieves a value from the database by its key.
+// It returns an error if the key is not found.
 func (impl *keyValueStorerImpl) Get(prefix, key string) ([]byte, error) {
 	return impl.Getf("%s-%s", prefix, key)
 }
 
+// Set sets a value in the database by its key.
+// It returns an error if the operation fails.
 func (impl *keyValueStorerImpl) Set(prefix, key string, val []byte) error {
 	return impl.Setf(val, "%s-%s", prefix, key)
 }
 
+// Delete deletes a value from the database by its key.
+// It returns an error if the operation fails.
 func (impl *keyValueStorerImpl) Delete(prefix, key string) error {
 	return impl.Deletef("%s-%s", prefix, key)
 }
+
+// Getf retrieves a value from the database by its key.
+// It returns an error if the key is not found.
 func (impl *keyValueStorerImpl) Getf(format string, a ...any) ([]byte, error) {
 	k := fmt.Sprintf(format, a...)
 	bin, err := impl.db.Get([]byte(k), nil)
@@ -56,6 +72,8 @@ func (impl *keyValueStorerImpl) Getf(format string, a ...any) ([]byte, error) {
 	return bin, nil
 }
 
+// Setf sets a value in the database by its key.
+// It returns an error if the operation fails.
 func (impl *keyValueStorerImpl) Setf(val []byte, format string, a ...any) error {
 	k := fmt.Sprintf(format, a...)
 	impl.db.Delete([]byte(k), nil)
@@ -66,6 +84,8 @@ func (impl *keyValueStorerImpl) Setf(val []byte, format string, a ...any) error 
 	return err
 }
 
+// Deletef deletes a value from the database by its key.
+// It returns an error if the operation fails.
 func (impl *keyValueStorerImpl) Deletef(format string, a ...any) error {
 	k := fmt.Sprintf(format, a...)
 	err := impl.db.Delete([]byte(k), nil)
@@ -75,6 +95,8 @@ func (impl *keyValueStorerImpl) Deletef(format string, a ...any) error {
 	return err
 }
 
+// View iterates over the key-value pairs in the database and calls the provided function for each pair.
+// It returns an error if the iteration fails.
 func (impl *keyValueStorerImpl) View(key string, processFunc func(key, value []byte) error) error {
 	iter := impl.db.NewIterator(nil, nil)
 	for ok := iter.Seek([]byte(key)); ok; ok = iter.Next() {
@@ -91,6 +113,9 @@ func (impl *keyValueStorerImpl) View(key string, processFunc func(key, value []b
 	return iter.Error()
 }
 
+// ViewFromFirst iterates over the key-value pairs in the database, starting from the first pair.
+// It calls the provided function for each pair.
+// It returns an error if the iteration fails.
 func (impl *keyValueStorerImpl) ViewFromFirst(processFunc func(key, value []byte) error) error {
 	iter := impl.db.NewIterator(nil, nil)
 	for ok := iter.First(); ok; ok = iter.Next() {
@@ -108,8 +133,9 @@ func (impl *keyValueStorerImpl) ViewFromFirst(processFunc func(key, value []byte
 	return iter.Error()
 }
 
-// Iterate function used to provide a list of key and values for your code
-// to iterate through.
+// Iterate iterates over the key-value pairs in the database, starting from the specified key prefix.
+// It calls the provided function for each pair.
+// It returns an error if the iteration fails.
 func (impl *keyValueStorerImpl) Iterate(keyPrefix string, seekThenIterateKey string, processFunc func(key, value []byte) error) error {
 	iter := impl.db.NewIterator(util.BytesPrefix([]byte(keyPrefix)), nil)
 
@@ -137,6 +163,9 @@ func (impl *keyValueStorerImpl) Iterate(keyPrefix string, seekThenIterateKey str
 	iter.Release()
 	return iter.Error()
 }
+
+// Close closes the database.
+// It returns an error if the operation fails.
 func (impl *keyValueStorerImpl) Close() error {
 	return impl.db.Close()
 }
