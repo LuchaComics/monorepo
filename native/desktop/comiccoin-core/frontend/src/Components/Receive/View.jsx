c579a824afabd9bb5030881424daf295e5521a0b
Bartlomiej Mika <bart@mikasoftware.com>
2024-10-23 14:38:42 -0400

    Latest progress with the receive page

--- native/desktop/comiccoin-core/frontend/src/Components/Receive/View.jsx
+++ native/desktop/comiccoin-core/frontend/src/Components/Receive/View.jsx
@@ -10,12 +10,32 @@ import {
   faCubes,
   faInbox
 } from "@fortawesome/free-solid-svg-icons";
+import { useRecoilState } from "recoil";
 
 import logo from '../../assets/images/CPS-logo-2023-square.webp';
 import {QRCodeSVG} from 'qrcode.react';
+import { currentOpenWalletAtAddressState } from "../../AppState";
 
 
 function ReceiveView() {
+    ////
+    //// Global State
+    ////
+
+    const [currentOpenWalletAtAddress] = useRecoilState(currentOpenWalletAtAddressState);
+
+    ////
+    //// Component states.
+    ////
+
+    ////
+    //// Event handling.
+    ////
+
+    ////
+    //// Misc.
+    ////
+
     useEffect(() => {
       let mounted = true;
 
@@ -27,6 +47,10 @@ function ReceiveView() {
         mounted = false;
       };
     }, []);
+
+    ////
+    //// Component rendering.
+    ////
 
     return (
         <>
@@ -44,17 +68,19 @@ function ReceiveView() {
                 <p class="has-text-grey">
                     Senders can scan this QRcode and quickly get a copy of your address.
                 </p>
+                <p class="has-text-grey">
+                  Your current wallet address is:
+                  <b> {currentOpenWalletAtAddress}</b>.
+                </p>
                   <p>&nbsp;</p>
                 <div className="columns is-centered">
                   <div class="column is-half">
                 <figure class="image">
                     {/* https://www.npmjs.com/package/qrcode.react */}
-                    <QRCodeSVG value="https://reactjs.org/" size={375} />
+                    <QRCodeSVG value={currentOpenWalletAtAddress} size={375} />
                 </figure>
                 </div>
                 </div>
-
-
 
               </nav>
             </section>
