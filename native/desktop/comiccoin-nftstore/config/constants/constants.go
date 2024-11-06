a27b783625c2c62d42a2033ddf5ba7aa8e6eb420
Bartlomiej Mika <bart@mikasoftware.com>
2024-11-06 15:10:05 -0500

    A bit of cleanup;

--- native/desktop/comiccoin-nftstore/config/constants/constants.go
+++ native/desktop/comiccoin-nftstore/config/constants/constants.go
@@ -5,15 +5,8 @@ import (
 )
 
 const (
-	ComicCoinChainID                        = constants.ChainIDMainNet
-	ComicCoinTransPerBlock                  = 1
-	ComicCoinDifficulty                     = 2
-	ComicCoinConsensusPollingDelayInMinutes = 1
-	ComicCoinConsensusProtocol              = constants.ConsensusPoA
-	ComicCoinPeerListenPort                 = 26644
-	ComicCoinBootstrapPeers                 = "/ip4/127.0.0.1/tcp/26642/p2p/QmZi2NPQx41oxfXdpVNGFtWp4rN1RJoYVyvAinKM9MHLh1" // Example `/ip4/127.0.0.1/tcp/26642/p2p/QmXYZ`.
-	ComicCoinIdentityKeyID                  = constants.DefaultIdentityKeyID
-	ComicCoinIPFSRemoteIP                   = "127.0.0.1"
-	ComicCoinIPFSRemotePort                 = "5002" // Temporary, please set back to on launch `5001`
-	ComicCoinIPFSPublicGatewayDomain        = "https://ipfs.io"
+	ComicCoinChainID                 = constants.ChainIDMainNet
+	ComicCoinIPFSRemoteIP            = "127.0.0.1"
+	ComicCoinIPFSRemotePort          = "5002" // Temporary, please set back to on launch `5001`
+	ComicCoinIPFSPublicGatewayDomain = "https://ipfs.io"
 )
