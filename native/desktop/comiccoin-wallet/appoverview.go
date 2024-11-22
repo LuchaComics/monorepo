37de84db4e2e8aad3a4353daa53349707bc77e01
Bartlomiej Mika <bart@mikasoftware.com>
2024-11-21 23:22:18 -0500

    More progress.

---
+++ native/desktop/comiccoin-wallet/appoverview.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"log/slog"
+	"strings"
+
+	"github.com/ethereum/go-ethereum/common"
+
+	"github.com/LuchaComics/monorepo/cloud/comiccoin-authority/domain"
+)
+
+func (a *App) GetTotalCoins(address string) (uint64, error) {
+	addr := common.HexToAddress(strings.ToLower(address))
+
+	// Defensive code
+	if address == "" {
+		return 0, fmt.Errorf("failed because: address is null: %v", address)
+	}
+
+	account, err := a.getAccountService.Execute(a.ctx, &addr)
+	if err != nil {
+		a.logger.Error("Failed getting account balance", slog.Any("error", err))
+		return 0, err
+	}
+
+	// Defensive code
+	if account == nil {
+		err := fmt.Errorf("Failed getting account because D.N.E. at address: %v", strings.ToLower(address))
+		a.logger.Error("Failed getting account balance",
+			slog.Any("error", err))
+		return 0, err
+	}
+
+	return account.Balance, nil
+}
+
+func (a *App) GetTotalTokens(address string) (int, error) { //TODO FINISH BELOW
+	// addr := common.HexToAddress(strings.ToLower(address))
+	//
+	// // Defensive code
+	// if address == "" {
+	// 	return 0, fmt.Errorf("failed because: address is null: %v", address)
+	// }
+	//
+	// tokCount, err := a.countByOwnerTokenService.Execute(a.ctx, &addr)
+	// if err != nil {
+	// 	a.logger.Error("Failed getting account balance", slog.Any("error", err))
+	// 	return 0, err
+	// }
+	//
+	// return tokCount, nil
+
+	return 0, nil //TODO: REPAIR ABOVE.
+}
+
+func (a *App) GetRecentTransactions(address string) ([]*domain.BlockTransaction, error) { //TODO FINISH BELOW
+	// addr := common.HexToAddress(strings.ToLower(address))
+	//
+	// // Defensive code
+	// if address == "" {
+	// 	return make([]*domain.BlockTransaction, 0), fmt.Errorf("failed because: address is null: %v", address)
+	// }
+	//
+	// txs, err := a.listRecentBlockTransactionService.Execute(&addr, 5)
+	// if err != nil {
+	// 	a.logger.Error("Failed getting account balance", slog.Any("error", err))
+	// 	return nil, err
+	// }
+	//
+	// return txs, nil
+	return make([]*domain.BlockTransaction, 0), nil //TODO: REPAIR ABOVE.
+}
